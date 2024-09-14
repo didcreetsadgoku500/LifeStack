cac3d71b0efb595d77d449872eab5170df7c38a0
Alex Peters <alexap9982@gmail.com>
2024-09-14 11:06:34 -0400

    Move connection string to env file, create sample calls

--- Express/app.ts
+++ Express/app.ts
@@ -1,16 +1,38 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
+const { Client } = require("pg");
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+const connectionURL = process.env.DATABASE_URL;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+const client = new Client(connectionURL);
+client.connect()
+
+app.get('/api/getCostOfLiving', (req, res) => {
+  const myResponse = {"key": "value", "key2": 2, "key3": [1,2,3]}
+
+  res.json(myResponse)
+  // res.send('Hello World!')
+})
+
+
+
+app.get('/api/databaseTest', async (req, res) => {
+  const results = await client.query(`
+    SELECT table_name 
+FROM information_schema.tables
+WHERE table_schema = 'public';
+    
+    `);
+  res.send(results.rows);
+})
+
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
+
