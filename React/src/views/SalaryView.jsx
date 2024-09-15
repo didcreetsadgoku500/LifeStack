cc2efe3ef41bfcb61477803b350cd349603d9219
Alex Englert <alex.englert.22@cnu.edu>
2024-09-14 20:48:51 -0400

    Update slarvyviw

--- React/src/views/SalaryView.jsx
+++ React/src/views/SalaryView.jsx
@@ -2,23 +2,26 @@ import { useState } from "react";
 import Input from "../atoms/Input"
 
 
-const handleSalaryChange = (value, salaryState, setSalary) => {
-    
+const handleSalaryChange = (e, salaryState, setSalary) => {
+    // TODO: enforce numbers only by comparing salaryState (previous state) to updated state
+    setSalary(e.target.value)
 }
 
 const SalaryView = () => {
-    const [salaryState, setSalary] = useState(null);
+    const [salaryState, setSalary] = useState("");
 
-    return <div className="lg:w-1/2 mx-auto pt-12">
+    return <div className="lg:w-1/3 mx-auto pt-12">
 
-    <h2 className="mb-4 text-5xl font-bold leading-none tracking-tight text-gray-800 md:text-5xl lg:text-5xl dark:text-white">Base Salary</h2>
-    <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"></p>
+    <h2 className="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-800 dark:text-white">Base Salary</h2>
+    <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl">Great! Now let's pin down your base salary. How much do you expect to make in a year before any expenses?</p>
+
 
 
     <div className="flex flex-wrap">
     <Input placeholder="Enter your salary" inputType="numeric" onInputChange={(e) => handleSalaryChange(e, salaryState, setSalary)}/>
-    <p>{salaryState}</p>
+
     </div>
+    <p>{salaryState}</p>
         
     </div>
           
