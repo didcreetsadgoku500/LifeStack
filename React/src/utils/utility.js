37b98e16e3b0ab1b0644497c69ff4b03b99302d8
Alex Peters <alexap9982@gmail.com>
2024-09-15 00:14:26 -0400

    Adds DC to getStateFullName

--- React/src/utils/utility.js
+++ React/src/utils/utility.js
@@ -8,6 +8,7 @@ export function getStateFullName(abbreviation) {
         CA: "California",
         CO: "Colorado",
         CT: "Connecticut",
+        DC: "District of Columbia",
         DE: "Delaware",
         FL: "Florida",
         GA: "Georgia",
