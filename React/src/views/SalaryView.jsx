8a9780e3b762f22700cd35ea204c1d87b67efc9e
Alex Englert <alex.englert.22@cnu.edu>
2024-09-14 21:17:23 -0400

    Add smart salary option detection

--- React/src/views/SalaryView.jsx
+++ React/src/views/SalaryView.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Input from "../atoms/Input"
 import CardButton from "../atoms/CardButton";
+import FadeTransition from "../components/FadeTransition";
 
 
 const handleSalaryChange = (e, salaryState, setSalary) => {
@@ -20,9 +21,13 @@ const SalaryView = () => {
 
     <div className="flex flex-wrap flex-col gap-5">
     <Input placeholder="Enter your salary" inputType="numeric" onInputChange={(e) => handleSalaryChange(e, salaryState, setSalary)}/>
-    <CardButton primary>
-        I don't know my salary
-    </CardButton>
+    <FadeTransition>
+        {(salaryState == "") ? 
+            <CardButton primary>I don't know my salary</CardButton> 
+        : 
+            <CardButton>Confirm salary</CardButton>    
+        }
+    </FadeTransition>
     </div>
     
         
