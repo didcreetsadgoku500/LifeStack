90295449c8226ff0cda207183f8375dfce369f05
Alex Peters <alexap9982@gmail.com>
2024-09-14 21:16:36 -0400

    Update CSS on CardButton

--- React/src/atoms/CardButton.tsx
+++ React/src/atoms/CardButton.tsx
@@ -8,17 +8,17 @@ interface ButtonProps {
 
 }
 
-const CardButton = ({onClick, children, iconSrc}: ButtonProps) => {
+const CardButton = ({onClick, children, iconSrc, primary}: ButtonProps) => {
 
 
     return <div className="group relative max-w-7xl mx-auto">
     <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-200 group-hover:duration-200 -z-10"></div>
-    <div className="px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-center justify-start space-x-6" onClick={onClick}>
+    <div className={primary ? "bg-gradient-to-r from-purple-600 to-pink-600 px-7 py-6 ring-1 ring-gray-900/5 rounded-lg leading-none flex items-center justify-start space-x-6 cursor-pointer" : "px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-center justify-start space-x-6 cursor-pointer"} onClick={onClick}>
       {
         iconSrc ? <img src={iconSrc} className="w-8 h-8"/> : ""
       }
       <div className="space-y-2">
-        <p className="text-slate-800 font-bold  ">{children}</p>
+        <p className={primary ? "text-slate-200 font-bold" : "text-slate-800 font-bold"}>{children}</p>
       </div>
     </div>
    </div>
